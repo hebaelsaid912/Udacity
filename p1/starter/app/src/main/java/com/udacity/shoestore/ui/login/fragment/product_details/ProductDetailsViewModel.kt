bd8f36f099aaf5ef29059e9b150836032b69759e
Heba Elsaid <hebaelsaid912@gmail.com>
2022-07-15 16:41:35 +0200

    handle shoe details data validation and add cancel btn

--- p1/starter/app/src/main/java/com/udacity/shoestore/ui/login/fragment/product_details/ProductDetailsViewModel.kt
+++ p1/starter/app/src/main/java/com/udacity/shoestore/ui/login/fragment/product_details/ProductDetailsViewModel.kt
@@ -1,9 +1,9 @@
 package com.udacity.shoestore.ui.login.fragment.product_details
 
 import android.app.Activity
-import android.content.Context
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -26,6 +26,9 @@ class ProductDetailsViewModel(val activity: Activity) : ViewModel() {
     val productDescription: MutableLiveData<String> by lazy {
         MutableLiveData<String>()
     }
+    fun onCancel(view: View){
+        activity.findNavController(viewId = view.id).navigateUp()
+    }
 
     fun onAddView(view: View) {
         val name = productName.value
@@ -37,18 +40,32 @@ class ProductDetailsViewModel(val activity: Activity) : ViewModel() {
         Log.d(TAG, "onAddView: company: $company")
         Log.d(TAG, "onAddView: size: $size")
         Log.d(TAG, "onAddView: description: $description")
-
-        activity.findNavController(viewId = view.id).navigate(
-            R.id.action_productDetailsFragment_to_homeFragment,
-            bundleOf(
-                "new_shoe" to Shoe(
-                    name = name.toString(),
-                    company = company.toString(),
-                    size = size!!.toDouble(),
-                    description = description.toString()
+        if (checkValidation(name.toString(),company.toString(),size.toString(),description.toString())){
+            Toast.makeText(activity.baseContext,"Data can't be empty",Toast.LENGTH_LONG).show()
+        }else {
+            activity.findNavController(viewId = view.id).navigate(
+                R.id.action_productDetailsFragment_to_homeFragment,
+                bundleOf(
+                    "new_shoe" to Shoe(
+                        name = name.toString(),
+                        company = company.toString(),
+                        size = size!!.toDouble(),
+                        description = description.toString()
+                    )
                 )
             )
-        )
+        }
 
+    }
+    private fun checkValidation(name:String ,company:String ,size:String ,description:String ):Boolean{
+        if(name == "null")
+            return true
+        if (company == "null")
+            return true
+        if(size == "null")
+            return true
+        if(description == "null")
+            return true
+        return false
     }
 }
